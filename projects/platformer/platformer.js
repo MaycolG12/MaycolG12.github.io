79063b6093b305182b7f4a1127d5e6a639f5f2e3
MaycolG12 Gamez <Maycolg127@gmail.com>
2023-09-01 13:02:00 +0000

    platformer

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -48,16 +48,17 @@ createPlatform(220,550,30,40);
 createPlatform(220,2,40,40);
 createPlatform(300, 550,30,3);
 createPlatform(80,80,1,100)
-createPlatform(500,600,20,7)
+createPlatform(500,585,20,7)
 createPlatform(400,300,100,10)
 createPlatform(710,500,200,10)
 createPlatform(220,1,5,560)
+createPlatform(600,370,50,10)
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-    createCollectable("diamond", 300, 480, 0, 0);
+    createCollectable("diamond", 300, 450, 0, 0);
     createCollectable("grace", 160, 340, 0, 0);
     createCollectable("max",800,400,0,0)
 
@@ -67,9 +68,9 @@ createPlatform(220,1,5,560)
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
 
-    createCannon("left", 600,980,60,50);
-    createCannon("left",300,400,60,50)
-    createCannon("top",580,1000,3,200) 
+    createCannon("left", 600,980000,50,50);
+    
+    createCannon("top",580,1100000,3,250) 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
